d204817c9e5d57fd0f2358f9f801215c1491dda8
NoeSourdes <sourdesnoe19@gmail.com>
2024-02-27 14:03:08 +0100

    Update CORS origin to localhost:3000

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const httpServer = http.createServer();
 
 const io = new Server(httpServer, {
   cors: {
-    origin: "https://navalrift.vercel.app",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST"],
     allowedHeaders: ["my-custom-header"],
     credentials: true,
