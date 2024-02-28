e8029aa04de2e2b1e122fe9ea68912597e79a230
NoeSourdes <sourdesnoe19@gmail.com>
2024-02-28 10:12:53 +0100

    Remove console.log statements and update server.js

--- server.js
+++ server.js
@@ -16,18 +16,11 @@ const io = new Server(httpServer, {
 io.on("connection", (socket) => {
   socket.on("join_conversation", (id_group) => {
     socket.join(id_group);
-    console.log(`L'utilisateur ${socket.id} a rejoint le groupe : ${id_group}`);
   });
 
   socket.on("send_msg", (data) => {
     console.log(data, "DATA");
-    //This will send a message to a specific room ID
     socket.to(data.id_group).emit("receive_msg", data);
-    console.log(
-      "Voici les utlisateur dans le groupe : ",
-      io.sockets.adapter.rooms.get(data.id_group)
-    );
-    console.log("Message envoyé au groupe : ", data.id_group);
   });
 
   socket.on("disconnect", () => {
@@ -36,6 +29,4 @@ io.on("connection", (socket) => {
 });
 
 const PORT = process.env.PORT || 3001;
-httpServer.listen(PORT, () => {
-  console.log(`Le serveur est en cours d'écoute sur le port ${PORT}`);
-});
+httpServer.listen(PORT, () => {});
